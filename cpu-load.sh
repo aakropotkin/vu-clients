311056ae2d0791fdecf8d235252a8b030ea027bf
Alex Ameen <alex.ameen.tx@gmail.com>
2024-08-17 13:42:50 -0500

    fix cpu-load.sh

--- cpu-load.sh
+++ cpu-load.sh
@@ -1,8 +1,9 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
-: "${MPSTAT:=mpstat}";
-: "${JQ:=jq}";
 : "${CURL:=curl}";
+: "${PS:=ps}";
+: "${GREP:=grep}";
+: "${BC:=bc}";
 
 : "${DIAL_ID:=4C0028000650564139323920}";
 : "${HUB_KEY:=cTpAWYuRpA2zx75Yh961Cg}";
@@ -10,14 +11,45 @@
 : "${SLEEP_SECONDS=0.3}";
 
 
-# Returns CPU Load as a percentage
-get_load() {
-  printf '%.*f\n' 0                                                            \
-    "$( $MPSTAT -o JSON|
-          $JQ '.sysstat.hosts[0].statistics[0]["cpu-load"][0]|.usr + .sys'; )";
+# Round a float to an integer
+round() {
+  if [[ "$#" -gt 0 ]]; then
+    while [[ "$#" -gt 0 ]]; do
+      printf '%.*f\n' 0 "$1";
+      shift;
+    done
+  else
+    local x;
+    while read -r x; do
+      printf '%.*f\n' 0 "$x";
+    done
+  fi
 }
 
-set_percent() {
+
+# Sum a set of integers
+sum() {
+  local _eqn="0";
+  if [[ "$#" -gt 0 ]]; then
+    while [[ "$#" -gt 0 ]]; do
+      _eqn="$_eqn + $1";
+      shift;
+    done
+  else
+    while read -r x; do
+      _eqn="$_eqn + $x";
+    done
+  fi
+  echo "$_eqn"|$BC -l;
+}
+
+
+# Returns CPU Load as a percentage
+get_cpu_load() {
+  $PS -eo pcpu|$GREP -v '^\(%CPU\| 0.0\)$'|sum|round;
+}
+
+set_cpu_load() {
   case "${1?You must pass a percentage as an integer}" in
     [0-9]|[1-9][0-9]|100) :; ;;
     *)
@@ -31,7 +63,7 @@ set_percent() {
 }
 
 # Set to a brightness ( default 20% )
-set_backlight() {
+set_cpu_backlight() {
   local -i brightness="${1:-20}";
   $CURL -X GET "$BASE_URL/api/v0/dial/$DIAL_ID/backlight?key=$HUB_KEY&\
 red=$brightness&blue=$brightness&green=$brightness";
@@ -42,13 +74,13 @@ red=$brightness&blue=$brightness&green=$brightness";
 declare -i prev=0;
 declare -i percent;
 
-set_percent 0;
-set_backlight;
+set_cpu_load 0;
+set_cpu_backlight;
 
 while :; do
-  percent="$( get_load; )";
+  percent="$( get_cpu_load; )";
   if [[ "$percent" -ne "$prev" ]]; then
-    set_percent "$percent";
+    set_cpu_load "$percent";
   fi
   prev="$percent";
   sleep "${SLEEP_SECONDS}s";
